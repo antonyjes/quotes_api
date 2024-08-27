fe7f75426609aa15832be8a9f520dd64bc18d0d8
antonyjes <72809149@continental.edu.pe>
2024-08-27 10:08:27 -0500

    start server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("Starting server on port 3000")
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+mongoose.set("strictQuery", false);
+
+// MONGODB CONNECTION
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
