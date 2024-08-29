5266faf1268e21dd82ed1f39a287ceb29f8ae3e5
antonyjes <72809149@continental.edu.pe>
2024-08-29 12:25:48 -0500

    main page

---
+++ client/src/pages/home/page.tsx
@@ -0,0 +1,13 @@
+import RootLayout from "../layout"
+
+const HomePage = () => {
+    return(
+        <RootLayout>
+            <div>
+                <p>Welcome to the app</p>
+            </div>
+        </RootLayout>
+    )
+}
+
+export default HomePage;
---
+++ client/src/pages/layout.tsx
@@ -0,0 +1,5 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="mx-auto max-w-7xl">{children}</div>;
+};
+
+export default RootLayout;
