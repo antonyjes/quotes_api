daf131c5d135cc11b7f8faaac9fd60529f002042
antonyjes <72809149@continental.edu.pe>
2024-08-29 13:19:03 -0500

    quote model

---
+++ server/models/Quote.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const QuoteSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    author: String,
+    submittedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Quote = mongoose.model("Quote", QuoteSchema);
+export default Quote;
