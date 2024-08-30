d8d2cb481076adf9461627acbdb24574164a8349
antonyjes <72809149@continental.edu.pe>
2024-08-30 12:17:21 -0500

    root layout

--- client/src/pages/home/page.tsx
+++ client/src/pages/home/page.tsx
@@ -1,12 +1,25 @@
-import { Navbar } from "@/components/navbar";
-import RootLayout from "../layout"
+import { Heading } from "@/components/heading";
+import RootLayout from "../layout";
+import { Separator } from "@/components/ui/separator";
 
 const HomePage = () => {
-    return(
-        <RootLayout>
-            <Navbar />
-        </RootLayout>
-    )
-}
+  const dateToday = new Date().toLocaleDateString("es-MX", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <RootLayout>
+      <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <Heading title="La frase de hoy" description={dateToday} />
+          <Separator />
+        </div>
+      </div>
+    </RootLayout>
+  );
+};
 
 export default HomePage;
