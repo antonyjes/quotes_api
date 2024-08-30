4dd13fa61927186d9cb3ed65bbce0053b3f6e659
antonyjes <72809149@continental.edu.pe>
2024-08-30 12:17:47 -0500

    navbar to the layout

--- client/src/pages/layout.tsx
+++ client/src/pages/layout.tsx
@@ -1,5 +1,12 @@
+import { Navbar } from "@/components/navbar";
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="mx-auto max-w-7xl">{children}</div>;
+  return (
+    <div className="mx-auto max-w-7xl">
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default RootLayout;
