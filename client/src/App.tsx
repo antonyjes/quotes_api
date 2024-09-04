e74d0351e662bc83035a757df0b47b5a75033f85
antonyjes <72809149@continental.edu.pe>
2024-09-04 13:20:14 -0500

    react toastify installed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,14 @@ import HomePage from "@/pages/home/page";
 import { ThemeProvider } from "@/components/theme-provider";
 import LoginPage from "@/pages/auth/login/page";
 import RegisterPage from "@/pages/auth/register/page";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ToastContainer />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
