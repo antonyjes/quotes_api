6535959c73f328873a784222bc63c71a896f52fb
antonyjes <72809149@continental.edu.pe>
2024-09-03 23:04:30 -0500

    dropdown responsive

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,14 +2,23 @@ import { cn } from "@/lib/utils";
 import { useLocation } from "react-router-dom";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useSelector } from "react-redux";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { User } from "@/lib/data";
-import { AlignJustifyIcon, Edit, LogOut } from "lucide-react";
+import { AlignJustifyIcon, Bookmark, Edit, LogOut } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
 
 export const Navbar = () => {
-  const isAuth = Boolean(useSelector((state: {token: string}) => state.token));
-  const user = useSelector((state: {user: User}) => state.user);
+  const isAuth = Boolean(
+    useSelector((state: { token: string }) => state.token)
+  );
+  const user = useSelector((state: { user: User }) => state.user);
 
   const routes = [
     {
@@ -61,28 +70,53 @@ export const Navbar = () => {
           <ModeToggle />
           {isAuth ? (
             <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <img className="w-[5rem] h-[5rem] rounded-full p-2 object-cover" src={`http://localhost:3003/assets/${user?.picturePath}`} alt={`${user?.firstName}`} />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>{user?.firstName} {user?.lastName}</DropdownMenuLabel>
-                    <DropdownMenuLabel>{user?.email}</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuLabel>Acciones</DropdownMenuLabel>
-                    <DropdownMenuItem>
-                        <Edit className="mr-2 h-4 w-4" />
-                        Editar perfil
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <LogOut className="mr-2 h-4 w-4" />
-                        Cerrar sesión
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
+              <DropdownMenuTrigger asChild>
+                <img
+                  className="w-[5rem] h-[5rem] rounded-full p-2 object-cover"
+                  src={`http://localhost:3003/assets/${user?.picturePath}`}
+                  alt={`${user?.firstName}`}
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>
+                  {user?.firstName} {user?.lastName}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                <DropdownMenuItem>
+                  <Bookmark className="mr-2 h-4 w-4" />
+                  Guardados
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Edit className="mr-2 h-4 w-4" />
+                  Editar perfil
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <LogOut className="mr-2 h-4 w-4" />
+                  Cerrar sesión
+                </DropdownMenuItem>
+              </DropdownMenuContent>
             </DropdownMenu>
           ) : (
             <nav className="flex items-center space-x-2 lg:space-x-4">
-                <a href="/login" className={cn(buttonVariants({ variant: "secondary", size: "sm"}), "px-4 py-2")}>Iniciar sesión</a>
-                <a href="/register" className={cn(buttonVariants({ variant: "default", size: "sm"}), "px-4 py-2")}>Registrarse</a>
+              <a
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4 py-2"
+                )}
+              >
+                Iniciar sesión
+              </a>
+              <a
+                href="/register"
+                className={cn(
+                  buttonVariants({ variant: "default", size: "sm" }),
+                  "px-4 py-2"
+                )}
+              >
+                Registrarse
+              </a>
             </nav>
           )}
         </div>
@@ -108,22 +142,18 @@ export const Navbar = () => {
                 </DropdownMenuItem>
               ))}
               <DropdownMenuSeparator />
-              {
-                isAuth ? (
-                    <DropdownMenuItem>
-                      Cerrar sesión
-                    </DropdownMenuItem>
-                ) : (
-                  <>
-                    <DropdownMenuItem>
-                      Iniciar sesión
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      Registrarse
-                    </DropdownMenuItem>
-                  </>
-                )
-              }
+              {isAuth ? (
+                <>
+                  <DropdownMenuItem>Guardados</DropdownMenuItem>
+                  <DropdownMenuItem>Editar perfil</DropdownMenuItem>
+                  <DropdownMenuItem>Cerrar sesión</DropdownMenuItem>
+                </>
+              ) : (
+                <>
+                  <DropdownMenuItem>Iniciar sesión</DropdownMenuItem>
+                  <DropdownMenuItem>Registrarse</DropdownMenuItem>
+                </>
+              )}
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
