850a4d7564526b29077007d2a59b2e96814d1071
antonyjes <72809149@continental.edu.pe>
2024-09-09 17:32:30 -0500

    quote interface

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -1,10 +1,22 @@
 export interface User{
+    _id: string
     firstName: string
     lastName: string
     email: string
     password: string
     picturePath: string
     role: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface Quote{
+    _id: string
+    content: string
+    author?: string
+    bgPath: string
+    topic: string
+    submittedBy?: string
     createdAt: Date
     updatedAt: Date
 }
