6d4f5cd48e967284e066d717957c01c6ea624335
antonyjes <72809149@continental.edu.pe>
2024-09-08 23:41:55 -0500

    comment

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ import { login, registerUser } from "../controllers/auth.js";
 
 const router = express.Router();
 
+// CREATE
 router.post("/login", login);
 router.post("/register", registerUser);
 
