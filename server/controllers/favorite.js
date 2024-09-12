5a8cdaf0b93dc422f28daa913921077834990696
antonyjes <72809149@continental.edu.pe>
2024-09-12 11:49:25 -0500

    update download and bookmark count

--- server/controllers/favorite.js
+++ server/controllers/favorite.js
@@ -1,4 +1,5 @@
 import Favorite from "../models/Favorite.js";
+import Quote from "../models/Quote.js";
 import User from "../models/User.js";
 
 export const addFavorite = async (req, res) => {
@@ -24,7 +25,29 @@ export const addFavorite = async (req, res) => {
             { new: true }
         );
 
+        await Quote.findOneAndUpdate(
+            { _id: quoteId },
+            { $inc: { savedCount: 1 } },
+            { new: true }
+        )
+
         res.status(201).json(savedFavorite);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const updateDownloadCount = async (req, res) => {
+    try {
+        const { quoteId } = req.body;
+        
+        await Quote.findOneAndUpdate(
+            { _id: quoteId },
+            { $inc: { downloadCount: 1 } },
+            { new: true }
+        )
+
+        res.status(200).json({ message: "Download count updated" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addFavorite } from "../controllers/favorite.js";
+import { addFavorite, updateDownloadCount } from "../controllers/favorite.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 // CREATE
-router.post("/save", addFavorite);
+router.post("/save", verifyToken, addFavorite);
+router.post("/update-download", verifyToken, updateDownloadCount);
 
 export default router;
 
