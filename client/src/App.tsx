5f50b2021d98709861ef4a36096e112fe41918e3
antonyjes <72809149@continental.edu.pe>
2024-09-13 17:13:10 -0500

    create quote client app route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,13 @@ import RegisterPage from "@/pages/auth/register/page";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 import QuotePage from "@/pages/quote/page";
+import { useSelector } from "react-redux";
+import { User } from "@/lib/data";
+import CreateQuotePage from "@/pages/quote/create";
 
 function App() {
+  const isAuth = Boolean(useSelector((state: { user: User }) => state.user));
+
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
@@ -18,6 +23,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/quotes/:quoteId" element={<QuotePage />} />
+            <Route path="/quotes/create" element={isAuth ? <CreateQuotePage /> : <LoginPage />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
